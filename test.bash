480bb052345a4cd333a50f00111fe2360f98e821
root <root@DESKTOP-V0CH8E9.localdomain>
2022-11-30 00:48:04 +0900

    add a command

--- test.bash
+++ test.bash
@@ -1,17 +1,26 @@
-#!/bin/bash -xv
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at Line $1
-	ews=1
+		echo NG at Line $1
+			res=1
 }
 
 res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng $LINENO
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
